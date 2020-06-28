689900340049bcefacf7c27b5d34debcb9c1d6a8
Olawumi <olawumi45@yahoo.com.sg>
2020-06-28 18:51:28 +0100

    code updated

--- university_data_scrapper.py
+++ university_data_scrapper.py
@@ -1,5 +1,5 @@
 #Import the necesary libraries
-import pandas as import pd
+import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 
@@ -7,7 +7,8 @@ url = 'https://en.wikipedia.org/wiki/List_of_universities_in_Nigeria'
 
 def scrapper():
     web_data = requests.get(url)
-    print(web_data)
+    soup = BeautifulSoup(web_data.content, parser = 'lxml', features= 'lxml')
+    print(soup.prettify)
 
 
 
